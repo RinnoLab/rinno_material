3b75fc1f9bd678df4ff19bd76721fa35ab6cbaef
Axel Díaz <diaz.axelio@gmail.com>
2018-01-12 13:02:42 -0300

    Test admin URL substitution

--- rinno_material/apps.py
+++ rinno_material/apps.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8
 from django.apps import AppConfig
+from .urlconf import ModuleURLResolver
 
 
 class RinnoMaterialConfig(AppConfig):
     name = 'rinno_material'
+
+    @property
+    def urls(self):  # noqa D102
+        return ModuleURLResolver(
+            r'^admin/', admin.site.urls[0], namespace='admin', module=self)
