041acc17a6add9343204906738990e2da38146f9
guslalo <gf.renjifo@gmail.com>
2018-01-25 09:51:04 -0300

    floating btn click

--- rinno_material/static/js/funciones.js
+++ rinno_material/static/js/funciones.js
@@ -160,7 +160,7 @@ $(".button-collapse2").sideNav({
 	mediaquery.addListener(handleOrientationChange);
 
 
-  $('.fixed-action-btn').openToolbar();
+  $('.fixed-action-btn > .btn-floating').click();
 
 
     // $("#loading").hide();
