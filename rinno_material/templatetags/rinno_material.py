7186e92a5cfec2009a19b39e27692b7ae101c4c9
Axel Díaz <diaz.axelio@gmail.com>
2018-01-30 13:37:34 -0300

    Añadiendo menú interno con templatetags

--- rinno_material/templatetags/rinno_material.py
+++ rinno_material/templatetags/rinno_material.py
@@ -1,7 +1,12 @@
 import re
+from collections import OrderedDict
+
 from django import template
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.utils.text import capfirst
 
 numeric_test = re.compile("^\d+$")
 register = template.Library()
@@ -28,6 +33,37 @@ def get_org_style(request, section=None, default_color=None):
         if current_organization:
             return getattr(current_organization.organizationstyle, section)
     return default_color
+
+
+@register.filter
+def applist(request):
+    app_dict = {}
+    user = request.user
+    site = admin.site
+    for model, model_admin in site._registry.items():
+        app_label = model._meta.app_label
+        has_module_perms = user.has_module_perms(app_label)
+
+        if has_module_perms:
+            perms = model_admin.get_model_perms(request)
+
+            if True in perms.values():
+                model_dict = {
+                    'name': capfirst(model._meta.verbose_name_plural),
+                    'admin_url': mark_safe('/admin/%s/%s/' % (app_label, model.__name__.lower())),
+                    'perms': perms,
+                }
+                if app_label in app_dict:
+                    app_dict[app_label]['models'].append(model_dict)
+                else:
+                    app_dict[app_label] = {
+                        'name': app_label.title(),
+                        'app_url': app_label + '/',
+                        'has_module_perms': has_module_perms,
+                        'models': [model_dict],
+                    }
+    app_list = OrderedDict(app_dict).values()
+    return app_list
 
 
 @register.filter
