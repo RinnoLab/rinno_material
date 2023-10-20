5445534ba2c4d263cb8d3dc9f16511beeb7b53c9
ulises <ulises.armenteros@rinno.la>
2023-10-20 12:29:24 -0400

    Upgrade

--- rinno_material/urls.py
+++ rinno_material/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from .views import PlayerID
 
 urlpatterns = [
-    path('update_player_id/(?P<onesignal_player_id>[0-9a-f-]+)',
-        PlayerID.as_view(),
-        name='update_player_id'),
+    path('update_player_id/<onesignal_player_id>/', PlayerID.as_view(), name='update_player_id'),
 ]
